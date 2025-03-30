6a1c08a0f5d401f5542ddba28d059c3aff9f0fad
lakshya <lakshyaparik99@gmail.com>
2025-03-30 08:36:03 +0530

    python functions

---
+++ Functions/fun_def/index.py
@@ -0,0 +1,13 @@
+def hello_world(intro):
+    print(f"hello world {intro}")
+    print("hello world")
+    print("hello world")
+
+#hello_world("I am steve")
+#hello_world("tea,coffee")
+
+def create_name(first,last):
+    full_name=first+" "+last
+    return full_name
+
+print(create_name("lakhay","pareek"))
