d7bc86f9c981a7f49747085a9bfaf8f6e7f15607
lakshya <lakshyaparik99@gmail.com>
2025-04-07 12:17:32 +0530

    __main__ for different usecases

---
+++ Files/if_name_is_main/greet.py
@@ -0,0 +1,7 @@
+def sayhello():
+    print("Hello, World!")
+
+
+if __name__=="__main__":
+    sayhello()
+    print("This is a test script.")
---
+++ Files/if_name_is_main/main.py
@@ -0,0 +1,4 @@
+# This is the main script that imports the greet module and calls its function
+import greet
+greet.sayhello()
+print("This is main.py")
